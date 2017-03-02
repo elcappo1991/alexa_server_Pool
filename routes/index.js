1c957a7894ae764a7b615b2b9edbf7fd7a7eed38
elcappo1991 <el.caappo@gmail.com>
2017-03-02 13:53:40 +0000

    the second commit

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,28 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var lockService=require('../services/lockService')
+var list =['living room','arpegio']
 
 /* GET home page. */
 router.get('/',function(req, res, next) {
 console.log('fn recalled')
-return 'hello world'
+res.send('GET')
 
 
 
 
 });
+
+
+router.post('/',function(req,res,next){
+	console.log(req.body.key)
+	if (list.indexOf(req.body.key) != -1 ){
+res.send('found')
+	}else {
+		res.send('not found')
+	}
+	
+})
 //get the home page function
 
 
