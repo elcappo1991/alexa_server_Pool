fe6f7af7e85415f4236627f477b316fbbef86851
elcappo1991 <el.caappo@gmail.com>
2017-03-29 18:02:25 +0100

    issue 14

--- routes/index.js
+++ routes/index.js
@@ -72,9 +72,12 @@ router.post('/playnext',function(req,res,next){
 			if (soc.linked ==true){
 					
 				 fn.sendSocketToSpeaker(soc.name,'play_next',function(result){
+				 	console.log('the result from bin/www is ',result)
 			      	if(result != false ){
+			      		console.log('if result is true')
 				 	 res.send({status:'ok'})	
 				 }else{
+				 	console.log('if result is false')
 				 	 res.send({status:'no'})	
 				 }
 			      	
@@ -98,8 +101,10 @@ router.post('/playnext',function(req,res,next){
      	else{
    			fn.sendSocketToSpeaker(req.body.key,'play_next',function(result){
 		      	if(result != false ){
+		      		console.log('if result is true')
 				 	 res.send({status:'ok'})	
 				 }else{
+				 	console.log('if result is false')
 				 	 res.send({status:'no'})	
 				 }
 			      	
